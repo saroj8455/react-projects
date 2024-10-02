31b40302d6533a533b78af9161ba6b4ee2b1bf34
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-10-02 19:32:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+import 'primeflex/primeflex.css'; // flex
+import Welcome from './components/Welcome';
+import Mainlayout from './Layout/Mainlayout';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import About from './pages/About';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Mainlayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: '*',
+        element: <ErrorPage />,
+      },
+    ],
+  },
+]);
+
+const App = () => <RouterProvider router={router} />;
+
+export default App;
