9eec81ada1cfbc1407ab6a429db880d60758b833
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-10-15 10:07:36 +0530

    update react-page with counter dependency api call

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,76 +1,78 @@
-import React from 'react';
-import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
-import { Chip } from 'primereact/chip';
+import React from "react";
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
+import { Chip } from "primereact/chip";
 
-import { Rating } from 'primereact/rating';
-export default function Post({ data }) {
+import { Rating } from "primereact/rating";
+function Post({ data }) {
   // check empty object and render a progress bar
   const checkEmpty = (params) => Object.keys(params).length === 0;
 
+  console.log(data);
+
   const header = (
     <img
-      alt='Card'
-      src='https://i.ytimg.com/vi/kkxE4ttcHUo/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLC5nYhB5h9XrIs7DmfLgDWf7G9dJA'
+      alt="Card"
+      src="https://i.ytimg.com/vi/kkxE4ttcHUo/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLC5nYhB5h9XrIs7DmfLgDWf7G9dJA"
     />
   );
   const footer = (
     <>
-      <Button className='w-full' label='Read more' />
+      <Button className="w-full" label="Read more" />
     </>
   );
 
   if (checkEmpty(data))
     return (
-      <div className='container text-2xl text-400'>
+      <div className="container text-2xl text-400">
         No data available! Please wait.....
       </div>
     );
 
   return (
-    <div className='container'>
-      <div className='card'>
+    <div className="container">
+      <div className="card">
         <Card
-          title='But Art is a punitive sentence, not a'
-          subTitle='powered by dummyjson api'
+          title={data.title}
+          subTitle="powered by dummyjson api"
           footer={footer}
           header={header}
-          className='md:w-25rem'
+          className="md:w-25rem"
         >
-          <div className='mb-4 flex gap-3'>
+          <div className="mb-4 flex gap-3">
             {data.tags.map((tag, index) => (
               <Chip key={index} label={tag} />
             ))}
           </div>
-          <div className='card flex align-items-center gap-1  mb-4'>
+          <div className="card flex align-items-center gap-1  mb-4">
             {/* <Rating value={data.rating} readOnly cancel={false} /> */}
             <Button
-              icon='pi pi-thumbs-up-fill'
+              icon="pi pi-thumbs-up-fill"
               label={data.reactions.likes}
               rounded
               text
-              severity='help'
-              aria-label='Favorite'
+              severity="help"
+              aria-label="Favorite"
             />
             <Button
-              icon='pi pi-thumbs-down-fill'
+              icon="pi pi-thumbs-down-fill"
               label={data.reactions.dislikes}
               rounded
               text
-              severity='help'
-              aria-label='Favorite'
+              severity="help"
+              aria-label="Favorite"
             />
             <Button
-              icon='pi pi-eye'
+              icon="pi pi-eye"
               label={data.views}
               rounded
               text
-              severity='help'
-              aria-label='Favorite'
+              severity="help"
+              aria-label="Favorite"
             />
           </div>
 
-          <p className='m-0  '>
+          <p className="m-0  ">
             {data.body.slice(0, data.body.length / 2)} ...
             {/* {data.body} */}
           </p>
@@ -79,3 +81,5 @@ export default function Post({ data }) {
     </div>
   );
 }
+
+export default React.memo(Post);
--- src/Pages/ReactPage.jsx
+++ src/Pages/ReactPage.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import Profile from '../Components/Profile';
-import Post from '../Components/Post';
-import useFetch from '../hooks/useFetch';
-import { Button } from 'primereact/button';
+import React, { useEffect, useState } from "react";
+import Profile from "../Components/Profile";
+import Post from "../Components/Post";
+import useFetch from "../hooks/useFetch";
+import { Button } from "primereact/button";
 
 export default function ReactPage() {
   const [post, setPost] = useState({});
   const [counter, setCounter] = useState(1);
   const dummyUrl = `https://dummyjson.com/posts/${counter}`;
-  const { data, loading, error } = useFetch(dummyUrl);
+  // const { data, loading, error } = useFetch(dummyUrl);
 
   const jhonProfile = {
-    title: 'Jhon Deo',
+    title: "Jhon Deo",
     showGithub: () => {
-      alert('github link');
+      alert("github link");
     },
   };
 
   const getPost = async () => {
     try {
-      const resp = await fetch('https://dummyjson.com/posts/9');
+      const resp = await fetch(`https://dummyjson.com/posts/${counter}`);
       if (!resp.ok) {
         // throw new Error("Failed to fetch data");
-        throw new Error('Failed to fetch data');
+        throw new Error("Failed to fetch data");
       }
       const data = await resp.json();
       setPost(data);
@@ -49,25 +49,25 @@ export default function ReactPage() {
 
   useEffect(() => {
     getPost();
-  }, []);
+  }, [counter]);
 
   return (
     <section>
-      <div className='container'>
-        <h3 className='text-2xl text-400'>ReactPage</h3>
+      <div className="container">
+        <h3 className="text-2xl text-400">ReactPage</h3>
         <hr />
         <Profile
           title={jhonProfile.title}
           showGithub={jhonProfile.showGithub}
         />
       </div>
-      <article className='mt-3'>
+      <article className="mt-3">
         <Post data={post} />
       </article>
-      <article className='mt-2'>
-        <div className='container'>
+      <article className="mt-2">
+        <div className="container">
           <h1>Current Post ID - {counter}</h1>
-          <Button label='Update Post ID' onClick={handelPostId} />
+          <Button label="Update Post ID" onClick={handelPostId} />
         </div>
       </article>
     </section>
